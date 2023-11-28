7fec291438d671b452ee3986d03f651b4df831c2
Athuljithpalakkal <ksathuljith@gmail.com>
2023-11-28 15:30:18 +0530

    Update Alldoctors.jsx

--- front-end/src/components/Alldoctors.jsx
+++ front-end/src/components/Alldoctors.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import Button from 'react-bootstrap/Button';
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import docImg from "../assets/alt_doc.jpeg"
+import docImg from "../assets/alt_doc.jpeg";
 import {
   MDBCard,
   MDBCardImage,
@@ -13,67 +13,61 @@ import {
   MDBListGroup,
   MDBListGroupItem,
   MDBRow,
-  MDBCol
-} from 'mdb-react-ui-kit';
+  MDBCol,
+} from "mdb-react-ui-kit";
 
 const Alldoctors = () => {
-    const [slots,setSlots] = useState([]);
-    const url = 'http://localhost:7890/api/admin/alldocs';
+  const [slots, setSlots] = useState([]);
+  // const url = 'http://localhost:7890/api/admin/alldocs';
+  const url = "https://pet-vet.onrender.com/api/admin/alldocs";
 
-    const getData = async()=>{
-      try {
-        const response = await axios.get(url);
-        // console.log(response);
-        const data = response.data.data;
-        // console.log(data);
-        setSlots(data)
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  const getData = async () => {
+    try {
+      const response = await axios.get(url);
+      // console.log(response);
+      const data = response.data.data;
+      // console.log(data);
+      setSlots(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    useEffect(()=>{
-      getData();
-    },[])
+  useEffect(() => {
+    getData();
+  }, []);
 
-    return (
-            
-        
-      <MDBRow className='row-cols-1 row-cols-md-3 row-cols-sm-2 g-4'>
+  return (
+    <MDBRow className="row-cols-1 row-cols-md-3 row-cols-sm-2 g-4">
       {slots && slots.length > 0 ? (
-              slots.map((item) => {
-                return (
-      <MDBCol>
-        <MDBCard>
-      <MDBCardImage position='top' src={`/images/${item.image}`} />
-      <MDBCardBody>
-        <MDBCardTitle>{item.doctor_name}</MDBCardTitle>
-        <MDBCardText>
-          {item.about}
-        </MDBCardText>
-      </MDBCardBody>
-      <MDBListGroup flush>
-        <MDBListGroupItem>Designation:{item.education}</MDBListGroupItem>
-        <MDBListGroupItem>Location:{item.location}</MDBListGroupItem>
-        <MDBListGroupItem>Phone:{item.phone}</MDBListGroupItem>
-      </MDBListGroup>
-      <MDBCardBody className='mx-auto'>
-        <MDBCardLink href={`/onedoc/${item._id}`}>view</MDBCardLink>
-      </MDBCardBody>
-    </MDBCard>
-    
-
-        </MDBCol>
-        
+        slots.map((item) => {
+          return (
+            <MDBCol>
+              <MDBCard>
+                <MDBCardImage position="top" src={`/images/${item.image}`} />
+                <MDBCardBody>
+                  <MDBCardTitle>{item.doctor_name}</MDBCardTitle>
+                  <MDBCardText>{item.about}</MDBCardText>
+                </MDBCardBody>
+                <MDBListGroup flush>
+                  <MDBListGroupItem>
+                    Designation:{item.education}
+                  </MDBListGroupItem>
+                  <MDBListGroupItem>Location:{item.location}</MDBListGroupItem>
+                  <MDBListGroupItem>Phone:{item.phone}</MDBListGroupItem>
+                </MDBListGroup>
+                <MDBCardBody className="mx-auto">
+                  <MDBCardLink href={`/onedoc/${item._id}`}>view</MDBCardLink>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
           );
         })
       ) : (
         <h1>No Doctors</h1>
       )}
-        </MDBRow>
+    </MDBRow>
+  );
+};
 
-      );
-      
-}
-
-export default Alldoctors
+export default Alldoctors;
