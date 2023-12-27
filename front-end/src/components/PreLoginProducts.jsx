7383917254926ad3f09d13164b71a76684063723
Athuljithpalakkal <ksathuljith@gmail.com>
2023-12-27 16:13:05 +0530

    changed backend to render

--- front-end/src/components/PreLoginProducts.jsx
+++ front-end/src/components/PreLoginProducts.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/Button";
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
-import {MDBIcon} from "mdb-react-ui-kit"
+import { MDBIcon } from "mdb-react-ui-kit";
 
 const PreLoginProducts = () => {
   const [slots, setSlots] = useState([]);
   const navigate = useNavigate();
-  const url = "http://localhost:7890/api/admin/allproducts";
+  // const url = "http://localhost:7890/api/admin/allproducts";
+  const url = "https://pet-vet.onrender.com/api/admin/allproducts";
 
   const getData = async () => {
     try {
@@ -55,30 +56,31 @@ const PreLoginProducts = () => {
       {/* Same as */}
       <ToastContainer />
       <Row xs={1} md={2} lg={3} className="g-4">
-      {slots && slots.length > 0 ? (
+        {slots && slots.length > 0 ? (
           slots.map((item) => {
             return (
-              <Col >
-      <Card style={{ width: '18rem' }} className="ml-auto mr-auto">
-      <Card.Img variant="top" src={`/imagesProduct/${item.image}`} />
-      <Card.Body>
-        <Card.Title>{item.product_name}</Card.Title>
-        <Card.Text>
-        {item.description}
-        </Card.Text>
-        <Card.Text>
-          {item.price}
-        </Card.Text>
-        <Button variant="primary" onClick={AtCart}>Add to cart</Button>
-      </Card.Body>
-    </Card>
-    </Col>
-     );
-    })
-  ) : (
-    <h1>No products at the moment</h1>
-  )}
-   </Row>
+              <Col>
+                <Card style={{ width: "18rem" }} className="ml-auto mr-auto">
+                  <Card.Img
+                    variant="top"
+                    src={`/imagesProduct/${item.image}`}
+                  />
+                  <Card.Body>
+                    <Card.Title>{item.product_name}</Card.Title>
+                    <Card.Text>{item.description}</Card.Text>
+                    <Card.Text>{item.price}</Card.Text>
+                    <Button variant="primary" onClick={AtCart}>
+                      Add to cart
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })
+        ) : (
+          <h1>No products at the moment</h1>
+        )}
+      </Row>
     </>
   );
 };
