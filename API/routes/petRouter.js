4dbaa02d734e15936d2177e9d3c17710e7e09a39
Athuljithpalakkal <ksathuljith@gmail.com>
2023-12-30 22:42:51 +0530

    testing checkauth

--- API/routes/petRouter.js
+++ API/routes/petRouter.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 
 // add pet booking
 
-petRouter.post("/addpet", checkAuth, (req, res) => {
+petRouter.post("/addpet", (req, res) => {
   const data = new pet({
     owner: req.body.owner,
     petname: req.body.petname,
