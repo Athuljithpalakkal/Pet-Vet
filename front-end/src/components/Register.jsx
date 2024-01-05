716a8c9e70bd0e0258adee1ae58c1fffce808c7e
Athuljithpalakkal <ksathuljith@gmail.com>
2024-01-05 12:43:50 +0530

    Update Register.jsx

--- front-end/src/components/Register.jsx
+++ front-end/src/components/Register.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 
 function Register() {
   const navigate = useNavigate();
-  // const url = "http://localhost:7890/api/register";
+
   const url = "https://pet-vet.onrender.com/api/register";
   const [slot, setSlot] = useState({
     name: "",
