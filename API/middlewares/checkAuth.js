b649332e7ceb959d272b3dcf248fce524f2da10f
Athuljithpalakkal <ksathuljith@gmail.com>
2024-01-05 12:20:32 +0530

    secrete key direct

--- API/middlewares/checkAuth.js
+++ API/middlewares/checkAuth.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
   try {
     console.log(req.headers.authorization);
     const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, process.env.secreteKey);
+    const decodedToken = jwt.verify(token, secret_this_should_be_longer);
     req.userData = {
       userId: decodedToken.userId,
       userName: decodedToken.userName,
