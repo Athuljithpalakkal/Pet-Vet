cd42aff069a297c34295bc500eea9de50338ef47
Athuljithpalakkal <ksathuljith@gmail.com>
2024-01-30 14:44:41 +0530

    Update HomeCard.js

--- front-end/src/components/HomeCard.js
+++ front-end/src/components/HomeCard.js
@@ -2,33 +2,26 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import "./Navbar.css";
-import AOS from "aos";
-import "aos/dist/aos.css";
 
 const HomeCard = () => {
-  useEffect(() => {
-    AOS.init();
-  }, []);
-
   return (
     <div>
       <div className="flex flex-col justify-center">
         <h2 className="mx-auto my-5 blueText">Our Services</h2>
 
-          <Card className="text-center w-3/5 mx-auto my-3 creamText bgBlue">
-            <Card.Header>Featured</Card.Header>
-            <Card.Body>
-              <Card.Title>Medical Clinic</Card.Title>
-              <Card.Text>
-                With supporting text below as a natural lead-in to additional
-                content.
-              </Card.Text>
-              <Button variant="light" className="bgCream blueText">
-                Go somewhere
-              </Button>
-            </Card.Body>
-          </Card>
-       
+        <Card className="text-center w-3/5 mx-auto my-3 creamText bgBlue">
+          <Card.Header>Featured</Card.Header>
+          <Card.Body>
+            <Card.Title>Medical Clinic</Card.Title>
+            <Card.Text>
+              With supporting text below as a natural lead-in to additional
+              content.
+            </Card.Text>
+            <Button variant="light" className="bgCream blueText">
+              Go somewhere
+            </Button>
+          </Card.Body>
+        </Card>
 
         {/* card 2 */}
         <Card className="text-center w-3/5 mx-auto my-3 creamText bgBlue">
