9b3f98e481d4c34cac2c81395d0898d54cfce125
Athuljithpalakkal <ksathuljith@gmail.com>
2024-01-30 17:24:21 +0530

    Update AllProducts.jsx

--- front-end/src/components/AllProducts.jsx
+++ front-end/src/components/AllProducts.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import Button from 'react-bootstrap/Button';
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import {
@@ -11,10 +11,11 @@ import {
   MDBCardLink,
   MDBBtn,
   MDBRow,
-  MDBCol
-} from 'mdb-react-ui-kit';
+  MDBCol,
+} from "mdb-react-ui-kit";
 
-{/* <Card.Img variant="top" src={`/imagesProduct/${item.image}`} />
+{
+  /* <Card.Img variant="top" src={`/imagesProduct/${item.image}`} />
           <ListGroup variant="flush">
             <ListGroup.Item>{item.product_name}</ListGroup.Item>
             <ListGroup.Item>Type :{item.type}</ListGroup.Item>
@@ -23,67 +24,61 @@ import {
           </ListGroup>
           <div className="flex flex-row justify-around">
           <Button className="w-3/5 my-1" variant="outline-primary" as={Link} to={`/oneproduct/${item._id}`}>view</Button>
-          </div> */}
-
-const AllProducts = () => {
-    const [slots,setSlots] = useState([]);
-    const url = 'http://localhost:7890/api/admin/allproducts';
-
-    const getData = async()=>{
-      try {
-        const response = await axios.get(url);
-        // console.log(response);
-        const data = response.data.data;
-        // console.log(data);
-        setSlots(data)
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    useEffect(()=>{
-      getData();
-    },[])
-
-    return (
-    <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
-    {slots && slots.length > 0 ? (
-            slots.map((item) => {
-              return (
-    <MDBCol>
-      <MDBCard>
-    <MDBCardImage position='top' alt='sorry bruh' src={`/imagesProduct/${item.image}`} />
-    <MDBCardBody>
-      <MDBCardTitle>{item.product_name}</MDBCardTitle>
-      <MDBCardText>
-      Type :{item.type}
-      </MDBCardText>
-    <MDBCardText>
-    Details :{item.details}
-      </MDBCardText>
-      <MDBCardText>
-      Price :{item.price}
-      </MDBCardText>
-      <MDBCardText>
-        {item.about}
-      </MDBCardText>
-    </MDBCardBody>
-    <MDBCardBody className='mx-auto'>
-      <MDBBtn href={`/oneproduct/${item._id}`}>View</MDBBtn>
-    </MDBCardBody>
-  </MDBCard>
-  
-
-      
-      </MDBCol>
-        );
-      })
-    ) : (
-      <h1>No Bookings</h1>
-    )}
-      </MDBRow>
-
-    );
+          </div> */
 }
 
-export default AllProducts
+const AllProducts = () => {
+  const [slots, setSlots] = useState([]);
+  // const url = 'http://localhost:7890/api/admin/allproducts';
+  const url = "https://pet-vet.onrender.com/api/admin/allproducts";
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(url);
+      // console.log(response);
+      const data = response.data.data;
+      // console.log(data);
+      setSlots(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <MDBRow className="row-cols-1 row-cols-md-3 g-4">
+      {slots && slots.length > 0 ? (
+        slots.map((item) => {
+          return (
+            <MDBCol>
+              <MDBCard>
+                <MDBCardImage
+                  position="top"
+                  alt="sorry bruh"
+                  src={`/imagesProduct/${item.image}`}
+                />
+                <MDBCardBody>
+                  <MDBCardTitle>{item.product_name}</MDBCardTitle>
+                  <MDBCardText>Type :{item.type}</MDBCardText>
+                  <MDBCardText>Details :{item.details}</MDBCardText>
+                  <MDBCardText>Price :{item.price}</MDBCardText>
+                  <MDBCardText>{item.about}</MDBCardText>
+                </MDBCardBody>
+                <MDBCardBody className="mx-auto">
+                  <MDBBtn href={`/oneproduct/${item._id}`}>View</MDBBtn>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          );
+        })
+      ) : (
+        <h1>No Bookings</h1>
+      )}
+    </MDBRow>
+  );
+};
+
+export default AllProducts;
